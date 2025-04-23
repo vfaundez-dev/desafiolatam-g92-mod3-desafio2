740ba580b0b2c76c7a95c7ff1282e6e035295fd8
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-04-23 10:57:49 -0400

    Completado desafio agregar y quitar borde a imagen

--- imagen_borde.js
+++ imagen_borde.js
@@ -4,4 +4,12 @@ const btnChangeStyle = document.querySelector("#btnChangeStyle");
 
 btnChangeStyle.addEventListener('click', function(event) {
 
+  if (imgChangeStyle.style.border == '2px solid red') {
+    imgChangeStyle.style.border = 'none';
+    btnChangeStyle.innerHTML = 'Agregar Borde'
+  } else {
+    imgChangeStyle.style.border = '2px solid red';
+    btnChangeStyle.innerHTML = 'Quitar Borde'
+  }
+  
 });
